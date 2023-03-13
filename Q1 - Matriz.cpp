8c7b201ac82accb9a7a519eafeae1fc4184378c9
Jussivan <115298615+Jussivan@users.noreply.github.com>
2023-03-13 19:22:27 -0300

    Update Q1 - Matriz.cpp

--- Q1 - Matriz.cpp
+++ Q1 - Matriz.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     int Matriz[3][3] = {{1, 2, 3},
-			            {4, 5, 6},
-                   	    {7, 8, 9}};
+			{4, 5, 6},
+                   	{7, 8, 9}};
     int Transposta[3][3];
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
